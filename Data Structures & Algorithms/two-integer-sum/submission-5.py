db2e24d56eaa19792b7da129c1fd406e7b567a5e
Sri_Harish <97216371+sri-harish-k@users.noreply.github.com>
2026-08-24 23:22:46 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    @staticmethod
+    def getNumsPosMap(nums: List[int]) -> dict[int, int]:
+        pos_map = {}
+
+        for i, num in enumerate(nums):
+            pos_map[num] = i
+        
+        return pos_map
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pos_map = Solution.getNumsPosMap(nums)
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            complement_pos = pos_map.get(complement, None)
+            if complement_pos:
+                # we don't want the case where nums[i] + nums[i] = target
+                if complement_pos != i:
+                    return [i, pos_map[complement]] 
+
+        # control never reaches here
+        return [-1, -1]
