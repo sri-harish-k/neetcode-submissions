34a6088b9aaa9ac7362a0c9e8d52b1b036f10ca8
Sri_Harish <97216371+sri-harish-k@users.noreply.github.com>
2026-08-29 16:04:22 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,24 @@
+from typing import List
+from collections import defaultdict
+class Solution:
+    @staticmethod
+    def getChCountMap(string: str) -> List[int]:
+        count_arr = [0] * 26 # index 0 -> count of a, index 1 -> count of b, ...index 25 - count of z
+
+        for ch in string:
+            key = ord(ch) - ord('a')
+            count_arr[key] += 1
+
+        return count_arr
+
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # key is the pattern (like 1 e, 1a, 1t) and value is the string that satisifies it (anagram)
+        category_map: dict[tuple, List] = defaultdict(list)
+
+        for string in strs:
+            ch_count_map = tuple(Solution.getChCountMap(string))
+
+            category_map[ch_count_map].append(string)
+
+        return list(category_map.values())
